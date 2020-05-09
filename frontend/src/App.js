4885f1813a08425fd7e48b66226160f15db50279
Niv <nivian.lee@gmail.com>
2020-05-09 20:02:27 +0800

    Setup reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-}
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route, Link, withRouter, Redirect } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
-export default App;
+import * as Reducer from './reducers/reducers.js';
+//for the appbar and drawer
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+
+const rootReducer = combineReducers({
+  reducer: Reducer.reducer,
+});
+
+const store = createStore(rootReducer);
+
+const App = (props) => {
+  return (
+    <Provider store={store}>
+      <Grid container direction='column'>
+        <Grid item xs={12} sm={12} md={12} lg={12}>
+          <Card>
+            <CardContent>Hello!</CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Provider>
+  );
+};
+
+export default withRouter(App);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
-import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
 
